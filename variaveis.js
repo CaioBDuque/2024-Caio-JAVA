742fb1653f7a878df2800c402b86f4889055a6ca
CaioBDuque <160872658+CaioBDuque@users.noreply.github.com>
2024-09-18 08:39:18 -0300

    Add files via upload

--- variaveis.js
+++ variaveis.js
@@ -16,3 +16,52 @@ console.log(soma)
 console.log(subtracao)
 console.log(multiplicacao)
 console.log(divisao)
+
+// Tipo Float -- ponto flutuante
+
+const numero04 = 11.7
+const numero05 = .7
+
+const multiplicacao2 = numero03 * numero05
+
+console.log(multiplicacao2)
+
+// NaN -> Not a Number
+
+const nome = 'Caio'
+const falsoNumero = '8'
+
+const naoEnumero = nome * 0.7
+const concatenacao01 = falsoNumero + numero04
+
+console.log(naoEnumero)
+console.log(concatenacao01)
+
+// Dados do tipo String = Textos
+
+const frase = 'Meu nome é: '
+const frase2 = 'O segundo DS é "estudioso", não é?'
+
+const concatenacao02 = frase + nome
+
+console.log(concatenacao02)
+console.log(frase2)
+
+// Dados do tipo Boolean
+// True -> verdadeiro
+// False -> Falso
+
+const numero06 = 8
+
+const comparacao01 = numero06 == falsoNumero
+const comparacao02 = numero06 === falsoNumero
+
+// Um sinal de igual (=) é para declarar
+// Dois sinais de igual (==) comparando so valores e não o tipo do dado (Comparação fraca)
+// Três sinais de igual (===) comparando os tipos de dados.(Comparação forte)
+
+console.log(comparacao01)
+console.log(comparacao02)
+
+
+
